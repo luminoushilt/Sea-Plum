46abdbc2b76e0a822f3abc11f874e6635d5581d5
Hilton Williams <hwilliams@luminoushilt.com>
2016-04-13 21:54:28 -0700

    fixing sea-plum landing page mobile nav bug.

--- _dev/Assets/js/script.js
+++ _dev/Assets/js/script.js
@@ -1,11 +1,75 @@
 jQuery.noConflict();
 var seaPlum = (function($) {
-    function name() {
 
+    // Toggles Mobile Menu
+    function mobileMenu() {
+        var $menuToggle = $('.mobile-nav-toggle');
+        var $menu = $('.navbar');
+        var $site = $('header');
+
+        $menuToggle.on('click', function() {
+            $(this).toggleClass('is-open');
+            $menu.toggleClass('nav-open');
+            $site.toggleClass('menu-open');
+        });
+    }
+
+    function backToTop() {
+        var $page = $('html, body');
+        var $back2top = $('.back-to-top');
+        var duration = 300;
+​
+        $back2top.click(function (jump) {
+            $page.animate({
+                scrollTop: 0
+            }, duration);
+            jump.preventDefault();
+        });
+​
+        $(window).scroll( function () {
+            var $self = $(this),
+                height = 0,
+                top = $self.scrollTop();
+​
+            console.log(top);
+​
+            if (top > height) {
+                if (!$back2top.is(':visible')) {
+                    $back2top.fadeIn(duration);
+                    }
+                } else {
+                    $back2top.hide();
+                }
+​
+            });
+    }
+
+    // Auto scrolling
+    function smoothScroll() {
+        var anchorTags = $('a[href*=#]:not([href=#])');
+        var duration = 300;
+
+        anchorTags.on('click', function() {
+            if(location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                var page = $('html, body');
+
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+
+                if(target.length) {
+                    page.animate({
+                        scrollTop: target.offset().top
+                    }, duration);
+                    return false;
+                }
+            }
+        });
     }
 
     $(document).ready(function() {
-        // Get Started
-
+        mobileMenu();
+        backToTop();
+        smoothScroll();
     });
+
 })(jQuery);
